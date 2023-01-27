59d7a278038b021e152381e49db2c3dafff9b7d7
Guilhem <117936340+Guiguicopter@users.noreply.github.com>
2023-01-27 18:57:08 +0100

    counter for "you've been trolled"

--- slippery-button/index.js
+++ slippery-button/index.js
@@ -1,6 +1,7 @@
 const hitbox = document.querySelector(".hitbox");
 const h2 = document.querySelector("h2");
 let hitboxPosition = { x: 50, y: 50 };
+let counter = 0;;
 
 function getRandomInt(min, max) {
   max = Math.floor(Math.random() * max) + min;
@@ -8,7 +9,10 @@ function getRandomInt(min, max) {
 }
 
 hitbox.addEventListener("mouseover", () => {
-  h2.style.visibility = "visible";
+  counter++
+  if (counter == 3) {
+    h2.style.visibility = "visible";
+  }
   hitboxPosition["x"] = getRandomInt(0, 100);
   hitboxPosition["y"] = getRandomInt(0, 100);
   hitbox.style.top = hitboxPosition["y"] + "%";
