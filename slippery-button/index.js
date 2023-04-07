0dbaa78eed58461b3d30db0523850ee388933f54
Guilhem <117936340+Guiguicopter@users.noreply.github.com>
2023-04-07 19:52:27 +0200

    multiples button but not working

--- slippery-button/index.js
+++ slippery-button/index.js
@@ -1,16 +1,38 @@
-const hitbox = document.querySelector(".hitbox");
-const h2 = document.querySelector("h2");
-let hitboxPosition = { x: 50, y: 50 };
-
 function getRandomInt(min, max) {
   max = Math.floor(Math.random() * max) + min;
   return max;
 }
 
-hitbox.addEventListener("mouseover", () => {
-  h2.style.visibility = "visible";
-  hitboxPosition["x"] = getRandomInt(0, 100);
-  hitboxPosition["y"] = getRandomInt(0, 100);
-  hitbox.style.top = hitboxPosition["y"] + "%";
-  hitbox.style.left = hitboxPosition["x"] + "%";
+class Button {
+  constructor(initial_pos_x, initial_pos_y, idNumber) {
+    this.element = document.createElement("div");
+    this.element.classList.add("hitbox");
+    this.element.id = `hitbox${idNumber}`;
+    this.element.innerHTML = '<button class="button"></button>';
+    document.body.appendChild(this.element);
+
+    this.hitboxPosition = { x: initial_pos_x, y: initial_pos_y };
+  }
+}
+
+const h2_1 = document.getElementById("h2-1");
+const h2_2 = document.getElementById("h2-2");
+const buttonList = {};
+let buttonNumber = 0;
+
+buttonList[`hitbox${buttonNumber}`] = new Button(50, 50, buttonNumber);
+buttonNumber++;
+
+let elements = Array.prototype.slice.call(document.querySelectorAll(".hitbox"));
+console.log(elements);
+
+elements.forEach((element) => {
+  element.addEventListener("mouseover", () => {
+    element.style.top = `${getRandomInt(0, 100)}%`;
+    element.style.left = `${getRandomInt(0, 100)}%`;
+    buttonList[`hitbox${buttonNumber}`] = new Button(50, 50, buttonNumber);
+    elements.push(buttonList[`hitbox${buttonNumber}`].element);
+    buttonNumber++;
+    console.log(elements);
+  });
 });
